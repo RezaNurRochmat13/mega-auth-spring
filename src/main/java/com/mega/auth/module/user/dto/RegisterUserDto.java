186b9949b5d208b7b0b12a534a95b9b2cd8a5027
RezaNurRochmat13 <rezanurrochmat@gmail.com>
2025-03-25 13:38:26 +0700

    (feat:auth): add validation password

--- src/main/java/com/mega/auth/module/user/dto/RegisterUserDto.java
+++ src/main/java/com/mega/auth/module/user/dto/RegisterUserDto.java
@@ -1,11 +1,18 @@
 package com.mega.auth.module.user.dto;
 
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class RegisterUserDto {
     private Long roleId;
     private String username;
+    @Size(min = 8, message = "Password must be at least 8 characters long")
+    @Pattern(
+            regexp = "^(?=.*[!@#$%^&*(),.?\":{}|<>]).*$",
+            message = "Password must contain at least one special character"
+    )
     private String password;
     private String email;
     private String phone;
